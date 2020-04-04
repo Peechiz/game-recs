8b91f7ef8636bbb25cb3576a317dbce424771279
Chris Impicciche <cimpicci@gmail.com>
2020-04-04 15:46:37 -0500

    updates jumbosrc

--- client/src/pages/AddEdit.jsx
+++ client/src/pages/AddEdit.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import GameDetail from "../components/gameDetail";
-import { chooseRandom } from "../util/util";
+import { getJumboSrc } from "../util/util";
 import "../assets/styles/AddEdit.css";
 import Form from "../components/form";
 
 const AddEdit = ({ entry, submitForm, back }) => {
   const { game } = entry;
 
-  const getJumboSrc = () =>
-    game.artworks
-      ? chooseRandom(game.artworks).url
-      : "https://www.placecage.com/800/400";
-
   return (
     <div>
       <div
         className="jumbotron jumbotron-fluid hero"
-        style={{ background: `url(${getJumboSrc()})` }}
+        style={{ background: `url(${getJumboSrc(game)})` }}
       />
       <div className="container-fluid">
         <div className="row">
