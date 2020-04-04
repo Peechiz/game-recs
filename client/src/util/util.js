4e0b93ac1fb41c962741ccaa3cf304d429e56d88
Chris Impicciche <cimpicci@gmail.com>
2020-04-04 15:42:56 -0500

    moves rando jumbotron to util

--- client/src/util/util.js
+++ client/src/util/util.js
@@ -1,3 +1,8 @@
 // util
 
 export const chooseRandom = arr => arr[Math.floor(Math.random() * arr.length)];
+
+export const getJumboSrc = game =>
+  (game && game.artworks)
+    ? chooseRandom(game.artworks).url
+    : "https://www.placecage.com/800/400";
